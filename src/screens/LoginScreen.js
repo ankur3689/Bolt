259be3d6806b096bbf89b28f77b7407a48eb6f23
ankur3689 <chandravanshi.ankur@gmail.com>
2022-07-20 15:34:15 +0530

    push

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -3,14 +3,14 @@ import {View, Text, StyleSheet, Button} from 'react-native';
 import useAuth from '../hooks/useAuth';
 
 const LoginScreen = ({navigation}) => {
-  const {auth,setAuth} = useAuth();
-  const loginUser = ()=>{
+  const {auth, setAuth} = useAuth();
+  const loginUser = () => {
     setAuth(true);
-  }
+  };
   return (
     <View style={styles.container}>
       <Text>LoginScreen</Text>
-      <Button title='Log In' onPress={()=>loginUser()}></Button>
+      <Button title="Log In" onPress={() => loginUser()}></Button>
     </View>
   );
 };
