fa94577c199b675dda48c0a8eecfe505e59e3134
ankur3689 <chandravanshi.ankur@gmail.com>
2022-07-20 14:58:00 +0530

    login setup

--- App.js
+++ App.js
@@ -8,20 +8,38 @@ import DrawerScreen from './src/screens/DrawerScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import LaunchScreen from './src/screens/LaunchScreen';
+import {AuthProvider} from './src/contexts/AuthContext';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import useAuth from './src/hooks/useAuth.js';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
+    <AuthProvider>
+      <SafeAreaProvider>
+        <AppNavigation />
+      </SafeAreaProvider>
+    </AuthProvider>
+  );
+};
+
+const AppNavigation = () => {
+  const {auth} = useAuth();
+  return (
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Launch"
         screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Launch" component={LaunchScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignUpScreen} />
-
-        <Stack.Screen name="DrawerScreen" component={DrawerScreen} />
+        {!auth ? (
+          <>
+            <Stack.Screen name="Launch" component={LaunchScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Signup" component={SignUpScreen} />
+          </>
+        ) : (
+          <Stack.Screen name="DrawerScreen" component={DrawerScreen} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
+import useAuth from '../hooks/useAuth';
 
 const LoginScreen = ({navigation}) => {
+  const {auth,setAuth} = useAuth();
+  const loginUser = ()=>{
+    setAuth(true);
+  }
   return (
     <View style={styles.container}>
       <Text>LoginScreen</Text>
-      <Button title='Log In' onPress={()=>navigation.navigate('DrawerScreen')}></Button>
+      <Button title='Log In' onPress={()=>loginUser()}></Button>
     </View>
   );
 };
