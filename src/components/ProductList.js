d727393ac23de63610a06e513f5ea3920fc29073
ankur3689 <chandravanshi.ankur@gmail.com>
2022-08-01 00:04:20 +0530

    activityindicator design centered

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {Text, StyleSheet, FlatList, View, Image} from 'react-native';
-import { set } from 'react-native-reanimated';
+import {
+  Text,
+  StyleSheet,
+  FlatList,
+  View,
+  Image,
+  ActivityIndicator,
+} from 'react-native';
+import {set} from 'react-native-reanimated';
 import useAxios from '../hooks/useAxios';
 
 const Item = ({item, onPress, backgroundColor, textColor}) => (
@@ -8,7 +15,7 @@ const Item = ({item, onPress, backgroundColor, textColor}) => (
     <View style={{flex: 2}}>
       <Image
         style={{width: '100%', height: '100%'}}
-        source={{uri:item.images[0].src}}
+        source={{uri: item.images[0].src}}
       />
     </View>
     <View style={{flex: 1}}>
@@ -19,23 +26,33 @@ const Item = ({item, onPress, backgroundColor, textColor}) => (
 );
 
 const ProductList = ({navigation}) => {
-  const [selectedId, setSelectedId] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const [products, setProducts] = useState([]);
   const axios = useAxios();
+  const loadProducts = async () => {
+    try {
+      let response = await axios.get('products');
+      setProducts(prev => {
+        return [...prev, ...response.data];
+      });
+      return 'success';
+    } catch (error) {
+      throw new Error('Error:' + error);
+    }
+  };
 
   useEffect(() => {
-    async function apiCall() {
-      try {
-        let response = await axios.get('products');
-        //console.log('headers:',response.data);
-        setProducts((prev)=>{
-          return [...prev,...response.data]
-        })
-      } catch (error) {
-        console.log('Error:',error);
-      }
-    }
-    apiCall();
+    setIsLoading(true);
+    loadProducts().then(
+      res => {
+        console.log('success', res);
+        setIsLoading(false);
+      },
+      err => {
+        console.log('tt', err);
+        setIsLoading(false);
+      },
+    );
   }, []);
 
   const renderItem = ({item}) => {
@@ -44,7 +61,6 @@ const ProductList = ({navigation}) => {
       <Item
         item={item}
         onPress={() => {
-          setSelectedId(item.id);
           navigation.navigate('ProductDetail');
         }}
       />
@@ -56,20 +72,35 @@ const ProductList = ({navigation}) => {
         data={products}
         renderItem={renderItem}
         keyExtractor={item => item.id}
-        extraData={selectedId}
+        extraData="extra_data"
         numColumns={2}
       />
+      {isLoading && (
+        <View
+          style={{
+            backgroundColor: 'rgba(52, 52, 52, 0.8)',
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            justifyContent: 'center',
+            alignItems: 'center',
+            opacity: 0.5,
+          }}>
+          <ActivityIndicator size="large" color="#fff" animating={isLoading} />
+        </View>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    borderRadius: 1,
-    borderColor: 'red',
-    borderWidth: 1,
+    //borderRadius: 1,
+    //borderColor: 'red',
+    //borderWidth: 1,
     flex: 1,
-    padding: 5,
   },
   item: {
     height: 300,
@@ -78,6 +109,8 @@ const styles = StyleSheet.create({
     flex: 1,
     borderColor: '#D3D3D3',
     borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
   },
   title: {
     fontSize: 16,
